b9bded7a5f3227613e4d894f07eb627179b02f30
AlAminKh03 <alaminkhan6203@gmail.com>
2022-05-07 16:54:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectID } = require('mongodb');
+require('dotenv').config();
+const jwt = require('jsonwebtoken')
+const port = process.env.port || 5000;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split('')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        console.log('decoded', decoded)
+    })
+    console.log(authHeader)
+    next()
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r4qkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect()
+        const serviceCollection = client.db('geniusCar').collection('service')
+        const orderCollection = client.db('geniusCar').collection('order')
+
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const email = req.query.email
+            if (email === decodedEmail) {
+                console.log(email)
+                const query = { email: email }
+                const cursor = orderCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectID(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+
+        })
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken })
+        })
+
+        app.post('/service', async (req, res) => {
+            const newService = req.body
+            const result = await serviceCollection.insertOne(newService)
+            res.send(result)
+        })
+
+        app.post('/order', async (req, res) => {
+            const newOrder = req.body
+            const result = await orderCollection.insertOne(newOrder)
+            res.send(result)
+        })
+
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectID(id) }
+            const result = await serviceCollection.deleteOne(query)
+            res.send(result)
+        })
+    }
+    finally { }
+}
+
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('server running')
+})
+
+app.listen(port, () => {
+    console.log("listening to port ")
+})
